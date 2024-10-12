85f3ea4b43f0aa54f22214e3213d60a3d2639d31
Pksharma8598 <68783019+Pksharma8598@users.noreply.github.com>
2024-10-12 11:36:50 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,12 +59,12 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
+            document.title = "Praveen Kumar";
             $("#favicon").attr("href", "assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "assets/images/favhand.png");
+            document.title = "Please come back";
+            $("#favicon").attr("href", "assets/images/hero.png");
         }
     });
 
